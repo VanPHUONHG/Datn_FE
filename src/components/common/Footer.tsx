e0100fd143da2a3314699ffa20f56919798bbc12
hatinh <tinhh02204@gmail.com>
2025-05-16 21:45:26 +0700

    Update Footer and Header content

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,14 +1,9 @@
- Tinh_Fe
+// src/components/common/Footer.tsx
 import React from 'react'
- main
 
 const Footer = () => {
   return (
- Tinh_Fe
     <div>Footer</div>
-
-    <div>footer</div>
- main
   )
 }
 
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,13 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
- Tinh_Fe
     <div>Header</div>
   )
-
-    <div>header</div>
-  );
- main
 }
 
 export default Header
